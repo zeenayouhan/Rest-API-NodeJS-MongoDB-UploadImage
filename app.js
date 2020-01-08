55fbf1aa220962586bbc172dcc178eb8b451e426
zeenayouhan <zinayouhan33@gmail.com>
2020-01-08 17:11:17 +0530

    sixthcommit

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express(); 
+const morgan = require('morgan');
 
 const productRoutes = require('./routes/product');
 const orderRoutes= require('./routes/order');
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 app.use('/product',productRoutes);
 app.use('/order',orderRoutes);
-app.use((req,res,err)=>{
-    res.status(202).json({
-        message: "It works",
 
-    });
+
+app.use((req,res,next)=>{
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
 });
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500 );
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
 
 module.exports= app;
--- routes/product.js
+++ routes/product.js
@@ -9,8 +9,13 @@ router.get('/product',(req,res,next)=>{
 });
 
 router.post('/',(req,res,next)=>{
-    res.status(200).json({
-        message:"Handling post request to /products"
+    const product ={
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message:"Handling post request to /products",
+        product: product
     });
 });
 
