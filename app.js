636ee87b83a34b532f2ba63ce77d7236ceef52d1
zeenayouhan <zinayouhan33@gmail.com>
2020-01-09 23:25:06 +0530

    Mongodb2

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express(); 
 const morgan = require('morgan');
+const mongoose=require('mongoose');
 
 const productRoutes = require('./routes/product');
 const orderRoutes= require('./routes/order');
 const bodyParser= require('body-parser');
-
+mongoose.connect("mongodb+srv://nodejs-image-upload:"+process.env.MONGO_ATLAS_PW+"@nodejs-image-upload-fglj0.mongodb.net/test?retryWrites=true&w=majority",{
+    useMongoClient:true
+})
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
